52d24a28ca74ff0c6840c091a1cd5f70c827e7a0
guowei <chuanjiabao1981@gmail.com>
2012-11-21 20:51:01 +0800

    tools script---mount dist

---
+++ tools/disk_mount.sh
@@ -0,0 +1,3 @@
+sudo mount /dev/xvdc1 /home/work/application/current/public/report_media/media_info_video/
+sudo mount /dev/xvdb1 /home/work/application/current/public/report_media/media_info_photo/
+
