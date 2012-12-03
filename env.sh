3c8fcfc443b1493afa6f8260bf26815b9fb048a5
root <root@localhost.(none)>
2012-12-03 13:48:39 +0000

    update install.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 CHECKAPP_USER=work
-CHECKAPP_SUDO_FILE=/etc/sudoers.d/checkapp.$CHECKAPP_USER
+CHECKAPP_SUDO_FILE=/etc/sudoers.d/checkapp_$CHECKAPP_USER
 CHECKAPP_MYSQL_CONF=/etc/mysql/conf.d/checkapp.cnf
 DATA_DIR=/home/$CHECKAPP_USER/mysql_data/
 
--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-. ../evn.sh
+. ../env.sh
 ##安转依赖的包
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion imagemagick libmagickcore-dev libmagickwand-dev nodejs libcurl4-openssl-dev  libcurl4-gnutls-dev 
 
